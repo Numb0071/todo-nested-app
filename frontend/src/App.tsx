d44c5473e6f2bec1455954f2290b0fe623478ee1
Numb0071 <48633625+Numb0071@users.noreply.github.com>
2026-03-15 19:48:24 +0100

    Update App.tsx to include the TodoTree component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoTree from "./components/TodoTree";
+
+function App() {
+  return (
+    <div>
+      <h1>ToDo App</h1>
+      <TodoTree />
+    </div>
+  );
+}
+
+export default App;
